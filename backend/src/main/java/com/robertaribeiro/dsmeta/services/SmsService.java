2a8f07e76c7321c324d395bd2a728c3fa75da182
HarukaIonaSao <robertaribeiro004@gmail.com>
2022-09-09 22:31:26 -0300

    Heroku

---
+++ backend/src/main/java/com/robertaribeiro/dsmeta/services/SmsService.java
@@ -0,0 +1,49 @@
+package com.robertaribeiro.dsmeta.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.robertaribeiro.dsmeta.entities.Sale;
+import com.robertaribeiro.dsmeta.repositories.SalesRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@Service
+public class SmsService {
+
+	@Value("${twilio.sid}")
+	private String twilioSid;
+
+	@Value("${twilio.key}")
+	private String twilioKey;
+
+	@Value("${twilio.phone.from}")
+	private String twilioPhoneFrom;
+
+	@Value("${twilio.phone.to}")
+	private String twilioPhoneTo;
+
+	@Autowired
+	private SalesRepository salesRepository;
+	
+	public void sendSms(Long saleId) {
+		
+		Sale sale = salesRepository.findById(saleId).get();
+		
+		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
+		
+		String msg= "O vendedor " + sale.getSellerName()+ " foi destaque em " + date
+				+ " com um total de R$ "+ String.format("%.2f", sale.getAmount());
+
+		Twilio.init(twilioSid, twilioKey);
+
+		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+		Message message = Message.creator(to, from, msg).create();
+
+		System.out.println(message.getSid());
+	}
+}
