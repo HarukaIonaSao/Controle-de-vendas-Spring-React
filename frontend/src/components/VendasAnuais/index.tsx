fd8d7ae21215f45d52b30c644aa1888f781189e7
HarukaIonaSao <robertaribeiro004@gmail.com>
2022-09-11 14:15:56 -0300

    Date update

--- frontend/src/components/VendasAnuais/index.tsx
+++ frontend/src/components/VendasAnuais/index.tsx
@@ -17,11 +17,16 @@ function VendasAnuais() {
 
     const [sales,setSales]= useState<Sale[]>([]);
     
-    useEffect(() => {
-      axios.get('${BASE_URL}/sales').then(response => {
+    useEffect(() => {      
+
+      const dmin = minDate.toISOString().slice(0, 10);
+      const dmax = maxDate.toISOString().slice(0, 10);
+
+
+      axios.get('${BASE_URL}/sales?=${dmin}&maxDate=${dmax}').then(response => {
         setSales(response.data.content);
       })
-    }, []);
+    }, [minDate, maxDate]);
     
 
   return (
